b8798eea4945d7dba6205309ad8e5705683f546c
Viniccius Wirz <vinimovel23@gmail.com>
2024-01-18 21:46:23 -0300

    feat: pyramid logic

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int num = 5;
+
+        for (int i = 1; i <= num; i++) {
+            StringBuilder pyramid = new StringBuilder();
+
+            for (int aux = 1; aux <= i; aux++) {
+                pyramid.append(i);
+            }
+
+            System.out.println(pyramid);
+        }
     }
 }
